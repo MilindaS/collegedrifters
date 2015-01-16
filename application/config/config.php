d55c7cd6993bdd0c1f9a843dd126bf2697050040
Milinda <rockmakas@gmail.com>
2015-01-16 13:54:47 +0530

    Modifiy files

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://collegedrifters.com/';
+$config['base_url']	= 'http://collegedrifters.loc/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/constants.php
+++ application/config/constants.php
@@ -40,4 +40,4 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
 
-define('BASEURL','http://www.collegedrifters.com/');
+define('BASEURL','http://www.collegedrifters.loc/');
