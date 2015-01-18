ad9b13570276ece85955bf5a99861f315a50f685
Milinda <rockmakas@gmail.com>
2015-01-18 11:29:04 +0530

    Modified design dash

--- application/modules/admin/views/dash.php
+++ application/modules/admin/views/dash.php
@@ -5,16 +5,118 @@
 		  <div id="total-visitor-snippet-title">TOTAL VISITORS</div>
 		   <div id="total-visitor-to-date">27/08/2014 15:23</div>
 		   <div id="total-visitor-count"  class="odometer"><center>0</center></div>
-		    </table>
+		  </div>
+		</div>
+	</div>
+
+	<div class="col-md-3">
+		<div class="panel panel-default admin-dash-snippet">
+		  <div class="panel-body">
+		  	<div class="row">
+		  		<div class="col-md-5 col-xs-5 col-sm-5" id="total-item-snippet-img">
+
+		  				<span class="glyphicon glyphicon-gift"></span>
+
+		  		</div>
+				<div class="col-md-7 col-xs-7 col-sm-7" id="total-item-snippet-text">
+					<div id="total-item-count">48</div>
+					<div id="total-item-snippet-title">Items</div>
+					<div id="total-item-sub-title">In collegedrifters</div>
+				</div>
+		  	</div>
+		  </div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default admin-dash-snippet">
+		  <div class="panel-body">
+		  	<div class="row">
+		  		<div class="col-md-5 col-xs-5 col-sm-5" id="total-user-snippet-img">
+
+		  				<span class="glyphicon glyphicon-user"></span>
+
+		  		</div>
+				<div class="col-md-7 col-xs-7 col-sm-7" id="total-user-snippet-text">
+					<div id="total-user-count">48</div>
+					<div id="total-user-snippet-title">Members</div>
+					<div id="total-user-sub-title">In collegedrifters</div>
+				</div>
+		  	</div>
+		  </div>
+		</div>
+	</div>
+
+	<div class="col-md-3">
+		<div class="panel panel-default admin-dash-snippet" id="snippet-green">
+		  <div class="panel-body">
+		  	<div class="row">
+		  		<div class="col-md-5 col-xs-5 col-sm-5" id="total-time-snippet-img">
+		  				<span class="glyphicon glyphicon-time"></span>
+		  		</div>
+				<div class="col-md-7 col-xs-7 col-sm-7" id="total-time-snippet-text">
+					<div id="time-sinppet">10:48</div>
+				</div>
+		  	</div>
+		  	<div class="row"><div class="col-md-12 col-sm-12 col-xs-12">
+		  		<div id="date-sinppet">In collegedrifters</div>
+		  	</div></div>
 		  </div>
 		</div>
 	</div>
 </div>
+
+
+
+
+
+
+
+
+
+
 <script>
-$(document).ready(function(){
+
 setTimeout(function(){
     $('.odometer').html(162);
-  }, 500);	
+  }, 500);
+
+$(document).ready(function(){
+	startTime();
+	
+
+	var mydate=new Date()
+    var year=mydate.getYear()
+
+    if (year < 1000)
+        year+=1900
+
+    var day=mydate.getDay()
+    var month=mydate.getMonth()
+    var daym=mydate.getDate()
+
+    if (daym<10)
+        daym="0"+daym
+
+    var dayarray=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday",
+                            "Friday","Saturday")
+    var montharray=new Array("January","February","March","April","May","June",
+                            "July","August","September","October","November","December")
+    $('#date-sinppet').html(dayarray[day]+", "+montharray[month]+" "+daym+", "+year);
 });
- 
+
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time-sinppet').innerHTML = h+":"+m;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
 </script>
