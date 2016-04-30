f3acf6476f82245cb4e444bbe7c3cd67260c7778
HLSMilinda <hlsmilinda@gmail.com>
2016-04-30 18:12:20 +0530

    Updated files

---
+++ application/logs/log-2016-04-30.php
@@ -0,0 +1,170 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-04-30 17:46:14 --> Config Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Hooks Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 17:46:14 --> URI Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Router Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> No URI present. Default controller set.
+DEBUG - 2016-04-30 17:46:14 --> Output Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Security Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Input Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-04-30 17:46:14 --> Language Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Language Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Config Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Loader Class Initialized
+DEBUG - 2016-04-30 17:46:14 --> Helper loaded: url_helper
+DEBUG - 2016-04-30 17:46:14 --> Database Driver Class Initialized
+ERROR - 2016-04-30 17:46:14 --> Severity: Warning  --> mysqli_connect(): (42000/1049): Unknown database 'colleged_db' /home/milinda/Projects/collegedrifters/system/database/drivers/mysqli/mysqli_driver.php 76
+ERROR - 2016-04-30 17:46:14 --> Unable to connect to the database
+DEBUG - 2016-04-30 17:46:14 --> Language file loaded: language/english/db_lang.php
+DEBUG - 2016-04-30 17:48:58 --> Config Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Hooks Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 17:48:58 --> URI Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Router Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> No URI present. Default controller set.
+DEBUG - 2016-04-30 17:48:58 --> Output Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Security Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Input Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-04-30 17:48:58 --> Language Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Language Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Config Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Loader Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Helper loaded: url_helper
+DEBUG - 2016-04-30 17:48:58 --> Database Driver Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Session Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Helper loaded: string_helper
+DEBUG - 2016-04-30 17:48:58 --> A session cookie was not found.
+DEBUG - 2016-04-30 17:48:58 --> Session routines successfully run
+DEBUG - 2016-04-30 17:48:58 --> Controller Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> Home MX_Controller Initialized
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/controllers/../modules/tracking/controllers/tracking.php
+DEBUG - 2016-04-30 17:48:58 --> Tracking MX_Controller Initialized
+DEBUG - 2016-04-30 17:48:58 --> Model Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/modules/tracking/models/mdl_tracking.php
+DEBUG - 2016-04-30 17:48:58 --> Model Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/modules/home/views/header.php
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/controllers/../modules/slider/controllers/slider.php
+DEBUG - 2016-04-30 17:48:58 --> Slider MX_Controller Initialized
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/modules/slider/models/mdl_slider.php
+DEBUG - 2016-04-30 17:48:58 --> Model Class Initialized
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/modules/home/views/home.php
+DEBUG - 2016-04-30 17:48:58 --> File loaded: application/modules/home/views/footer.php
+DEBUG - 2016-04-30 17:48:58 --> Final output sent to browser
+DEBUG - 2016-04-30 17:48:58 --> Total execution time: 0.0904
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> No URI present. Default controller set.
+DEBUG - 2016-04-30 18:03:53 --> Output Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Security Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Input Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-04-30 18:03:53 --> Language Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Language Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Loader Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Helper loaded: url_helper
+DEBUG - 2016-04-30 18:03:53 --> Database Driver Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Session Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Helper loaded: string_helper
+DEBUG - 2016-04-30 18:03:53 --> Session routines successfully run
+DEBUG - 2016-04-30 18:03:53 --> Controller Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Home MX_Controller Initialized
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/controllers/../modules/tracking/controllers/tracking.php
+DEBUG - 2016-04-30 18:03:53 --> Tracking MX_Controller Initialized
+DEBUG - 2016-04-30 18:03:53 --> Model Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/modules/tracking/models/mdl_tracking.php
+DEBUG - 2016-04-30 18:03:53 --> Model Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/modules/home/views/header.php
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/controllers/../modules/slider/controllers/slider.php
+DEBUG - 2016-04-30 18:03:53 --> Slider MX_Controller Initialized
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/modules/slider/models/mdl_slider.php
+DEBUG - 2016-04-30 18:03:53 --> Model Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/modules/home/views/home.php
+DEBUG - 2016-04-30 18:03:53 --> File loaded: application/modules/home/views/footer.php
+DEBUG - 2016-04-30 18:03:53 --> Final output sent to browser
+DEBUG - 2016-04-30 18:03:53 --> Total execution time: 0.0631
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+ERROR - 2016-04-30 18:03:53 --> 404 Page Not Found --> public/uploads/itempics/3_1441913241.jpg
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+ERROR - 2016-04-30 18:03:53 --> 404 Page Not Found --> public/uploads/itempics/3_1441913198.jpg
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+ERROR - 2016-04-30 18:03:53 --> 404 Page Not Found --> public/uploads/itempics/3_1446615872.png
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+ERROR - 2016-04-30 18:03:53 --> 404 Page Not Found --> public/uploads/itempics/3_1441913198.jpg
+ERROR - 2016-04-30 18:03:53 --> 404 Page Not Found --> public/uploads/itempics/3_1441913241.jpg
+DEBUG - 2016-04-30 18:03:53 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:53 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:53 --> Router Class Initialized
+ERROR - 2016-04-30 18:03:53 --> 404 Page Not Found --> public/uploads/itempics/3_1446615872.png
+DEBUG - 2016-04-30 18:03:54 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Hooks Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Utf8 Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> UTF-8 Support Enabled
+DEBUG - 2016-04-30 18:03:54 --> URI Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Router Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Output Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Security Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Input Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-04-30 18:03:54 --> Language Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Language Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Config Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Loader Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Helper loaded: url_helper
+DEBUG - 2016-04-30 18:03:54 --> Database Driver Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Session Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Helper loaded: string_helper
+DEBUG - 2016-04-30 18:03:54 --> Session routines successfully run
+DEBUG - 2016-04-30 18:03:54 --> Controller Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> Login MX_Controller Initialized
+DEBUG - 2016-04-30 18:03:54 --> File loaded: application/controllers/../modules/home/controllers/home.php
+DEBUG - 2016-04-30 18:03:54 --> Home MX_Controller Initialized
+DEBUG - 2016-04-30 18:03:54 --> File loaded: application/controllers/../modules/tracking/controllers/tracking.php
+DEBUG - 2016-04-30 18:03:54 --> Tracking MX_Controller Initialized
+DEBUG - 2016-04-30 18:03:54 --> Model Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> File loaded: application/modules/tracking/models/mdl_tracking.php
+DEBUG - 2016-04-30 18:03:54 --> Model Class Initialized
+DEBUG - 2016-04-30 18:03:54 --> File loaded: application/modules/home/views/header.php
+DEBUG - 2016-04-30 18:03:54 --> File loaded: application/modules/login/views/login.php
+DEBUG - 2016-04-30 18:03:54 --> File loaded: application/modules/home/views/footer.php
+DEBUG - 2016-04-30 18:03:54 --> Final output sent to browser
+DEBUG - 2016-04-30 18:03:54 --> Total execution time: 0.1120
